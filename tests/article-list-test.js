a416151131119b6edbe22c9098b0c463ac50f829
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-09 16:33:35 +0000

    Updates spec for ArticleList Model. Issues with TestRunner not showing my tests

--- tests/article-list-test.js
+++ tests/article-list-test.js
@@ -1,10 +1,20 @@
 function AddArticleToArticleList(){
   var describe = "Articles added to Article list"
-  var articleList = new ArticleList();
-  articleList.addNewArticle("headline", "summary");
-  articleList.addNewArticle("headline", "summary");
-  console.log(articleList.viewArticles());
-  assert.isTrue(articleList.viewArticles().length === 2, describe);
+  var articlelist = new ArticleList();
+  articlelist.addArticle("Storm Ciara", "Hurricane-force winds batter UK transport network");
+  articlelist.addArticle("Coronavirus", "Britons arrive from Wuhan as deathtoll overtakes Sars");
+  console.log(articlelist.viewArticles());
+  console.log(articlelist.viewArticles()[0]);
+  console.log(articlelist.viewArticles()[0].headline); // headline for storm ciara
+  console.log(articlelist.viewArticles()[0].content);
+  console.log(articlelist.viewArticles()[1].headline); // headline for coronavirus
+  console.log(articlelist.viewArticles()[1].content);
+  console.log(articlelist.viewArticles()[0].headline === "Storm Ciara");
+  console.log(articlelist.viewArticles()[1].content === "Britons arrive from Wuhan as deathtoll overtakes Sars");
+  console.log(articlelist.viewArticles().length === 2);
+  assert.isTrue(articlelist.viewArticles().length === 2, describe);
+  assert.isTrue(articlelist.viewArticles()[0].headline === "Storm Ciara", describe);
+  assert.isTrue(articlelist.viewArticles()[1].content === "Britons arrive from Wuhan as deathtoll overtakes Sars", describe);
 }
 
 AddArticleToArticleList();
